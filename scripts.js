ce3db3bfecbd2e5f0f9dc11d903ab8b7e7e01eb1
MarcosPaulobgs <147336608+MarcosPaulobgs@users.noreply.github.com>
2026-03-02 20:57:44 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Função chamada "comprar"
+function comprar() {
+
+    // Exibe uma caixa de alerta na tela do usuário
+    // O texto dentro do alert é a mensagem que será mostrada
+    alert("Você será direcionado para o WhatsApp para finalizar o pedido!");
+    
+} // Fim da função
+
